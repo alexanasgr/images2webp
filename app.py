4ea2b07249c28f1f97f93a67ef9c1ec2d10a194c
alexanastasgr <alexanastasgr@gmail.com>
2022-11-03 13:27:39 +0200

    add some controls and convert method

--- app.py
+++ app.py
@@ -1,25 +1,50 @@
-import os.path
+import os
+from os import listdir
 
-# This App converts all images from source Folder
+# This 🐍 App converts all images from source Folder
 #  to modern WEBP format into output directory
 
-thefolders = ["source","output"]
+sourceDir = "source"
+outputDir = "output"
 
 
 # check if folders  exists
-def checkPathExists(folders):
+def checkPathExists(sourceDir,outputDir):
+
+    source =  os.path.isdir(sourceDir)
+    output =  os.path.isdir(outputDir)
     
-    for folder in folders:
-        if(os.path.isdir(folder)):
-            convertImages()
-     
+    if((source) == True & (output) == True):
+        
+        # if folders exists load images from source
+         loadImagesFromSource()
+        
+        # otherwise return error
+    else:
+        print("Something went wrong......")
+            
+        
+    
 # the actual convertation function
-def convertImages():
-    print("Now converting images.....")    
+def loadImagesFromSource():
+    
+    for images in os.listdir(sourceDir):
+         if (images.endswith(".jpg")):
+            
+             # now convert those images to webp
+             convertSupportedImages(images)
+             
+            
+# the convertion method        
+def convertSupportedImages(images):
+    for img  in images:
+        print(f"Image: {img} converted  success..")
+           
+    
 
 # main method here
 def main():
-   checkPathExists(thefolders)
+   checkPathExists(sourceDir,outputDir)
 
 # some extra 🔒 for this app
 if __name__ == '__main__':
